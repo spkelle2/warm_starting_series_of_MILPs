0c15a305c51eafc648c4b3d2f8d335d1fa52899e
Sean <seanpkelley94@gmail.com>
2022-12-13 16:16:11 -0600

    updated coral batch runner

--- experiments/batch_runner.py
+++ experiments/batch_runner.py
@@ -17,11 +17,8 @@ def run_batch(instance_fldr: str, data_fldr: str, disjunctive_term_list: List[in
     combos = list(product(os.listdir(instance_fldr), disjunctive_term_list))
     for i, (instance, disjunctive_terms) in enumerate(combos):
         print(f'\n\nrunning experiment {i + 1} of {len(combos)}\n\n')
+        instance_name = instance.split('.')[0]
         instance_pth = os.path.join(instance_fldr, instance)
-        # run_experiment(instance_pth=instance_pth, data_fldr=data_fldr,
-        #                disjunctive_terms=disjunctive_terms, max_cut_generators=max_cut_generators,
-        #                mip_gap=mip_gap, min_progress=min_progress, time_limit=time_limit,
-        #                log=log)
         if not run_pbs:
             subprocess.call(['python', 'experiment.py', instance_pth, data_fldr,
                              str(disjunctive_terms), str(max_cut_generators),
@@ -31,12 +28,13 @@ def run_batch(instance_fldr: str, data_fldr: str, disjunctive_term_list: List[in
                       f'disjunctive_terms=${disjunctive_terms},' \
                       f'max_cut_generators=${max_cut_generators},mip_gap=${mip_gap},' \
                       f'min_progress=${min_progress},time_limit=${time_limit},log=${log}'
-            subprocess.call(['qsub', '-q', 'batch', '-l', 'ncpus=4,mem=15gb,vmem=15gb,pmem=15gb',
-                             '-v', arg_str, 'submit.pbs'])
+            subprocess.call(['qsub', '-q', 'batch', '-l', 'ncpus=4,mem=7gb,vmem=7gb,pmem=7gb',
+                             '-v', arg_str, '-e', f'{instance_name}.err',
+                             '-o', f'{instance_name}.out', 'submit.pbs'])
 
 
 if __name__ == '__main__':
     wkdir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
-    run_batch(instance_fldr=os.path.join(wkdir, 'instances/small'),
-              data_fldr=os.path.join(wkdir, 'data/small'),
+    run_batch(instance_fldr=os.path.join(wkdir, 'instances/tiny'),
+              data_fldr=os.path.join(wkdir, 'data/tiny'),
               disjunctive_term_list=[4, 8, 16, 32], log=0, run_pbs=True)
